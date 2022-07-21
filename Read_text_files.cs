d58dd8e16034cec59590f2f4b63df041c376d6df
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-21 14:52:48 +0530

    Add files via upload

---
+++ Read_text_files.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access_text_files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\C#_CONCEPTS_Screenshots\\c#_data_access_text_files\\C_sharp_data_access.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList(); 
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
