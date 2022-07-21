0af257f1668848fd27e4811fd381f919ea73c885
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-21 14:59:23 +0530

    Add files via upload

---
+++ Write_in_a_text_files.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access_text_files
+{
+    internal class Read_text_files
+    {
+        static void Main(string[] args)
+        {//codes for reading from a text file
+            string filePath = "D:\\C#_CONCEPTS_Screenshots\\c#_data_access_text_files\\C_sharp_data_access.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList(); 
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            //codes for writing in a text file
+            lines.Add("This is the first line that is written in ide.");
+            File.WriteAllLines(filePath, lines.ToArray());
+            Console.ReadLine();
+
+        }
+    }
+}
